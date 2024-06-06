869acaca8c1a1b102b567fefe2a3232435753ec3
Zongrui Ma <theoneuma9@gmail.com>
2024-06-06 13:01:33 -0400

    add middleware to auth request

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export async function middleware(request: NextRequest) {
+
+
+
+    const requestHeaders = new Headers(request.headers)
+    requestHeaders.set('x-middleware-test', 'hello')
+    const sec = requestHeaders.get('Coding-Clubs')
+    if (sec !== process.env.REQ_HEADER_AUTH) {
+        return new NextResponse(
+            JSON.stringify({
+                message: 'Ha Ha Ha Ha Fool!'
+            }),
+            {
+                status: 404,
+            })
+    }
+    const response = NextResponse.next({
+        headers: requestHeaders
+    })
+    response.headers.set('x-middleware-test', 'hello')
+    return response
+    
+    
+}
